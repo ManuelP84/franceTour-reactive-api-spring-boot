8f48a46cda046b91a40f43e33eab6e69afc733a6
Manuel Pineda <pineda.manuel@gmail.com>
2022-07-19 08:14:58 -0500

    Start franceTour api

---
+++ src/main/java/com/sofka/franceTour/FranceTourApplication.java
@@ -0,0 +1,13 @@
+package com.sofka.franceTour;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FranceTourApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FranceTourApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sofka/franceTour/FranceTourApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sofka.franceTour;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FranceTourApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
